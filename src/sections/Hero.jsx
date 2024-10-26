9250f69afde5112f1b3c513c68169f96f50d639b
Arthur <arthurndhlovu8@gmail.com>
2024-10-26 20:14:25 +0200

    Hero Section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -8,18 +8,19 @@ const Hero = () => {
         <div className="container">
           <div className="relative z-2 max-w-512 max-lg:max-w-388">
             <div className="caption small-2 uppercase text-p3">
-              Video Editing
+              IT Support
             </div>
             <h1 className="mb-6 h1 text-p4 uppercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">
               Amazingly simple
             </h1>
             <p className="max-w-440 mb-14 body-1 max-md:mb-10">
-              We designed XORA AI Video Editor to be an easy to use, quick to
-              learn, and surprisingly powerful.
+              We know IT systems are critical for commercial success – but we also know that for small and medium businesses maintaining them can be expensive. 
+              That’s why we’ll help to keep your costs down, without compromising on quality or reliability.
             </p>
-            <LinkScroll to="features" offset={-100} spy smooth>
-              <Button icon="/images/zap.svg">Try it now</Button>
-            </LinkScroll>
+            <a href="mailto:arthurndhlovu8@gmail.com">
+              <Button icon="/images/zap.svg">Contact Us now</Button>
+            </a>
+
           </div>
 
           <div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
